853ae61901da922b263355c3cd990e81ca5df202
Icaro Cavalcanti <icaro.jmc@gmail.com>
2020-11-28 16:21:18 -0300

    C001. Sem código, é apenas para sincronizar os repositórios.

---
+++ src/main/java/com/icarocavalcanti/leitordecnis2/LeitorPorArea.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.icarocavalcanti.leitordecnis2;
+
+/**
+ *
+ * @author Ícaro
+ */
+public class LeitorPorArea {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
